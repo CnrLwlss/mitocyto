aa92170dcd55e9841392ea888ec841cb4dc951a2
Conor Lawless <cnr.lwlss@gmail.com>
2019-07-09 14:37:44 +0100

    New version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import os
 
-version='0.0.15'
+version='0.0.16'
 f=open('mitocyto/version.py',"w")
 f.write("__version__='{}'".format(version))
 f.close()
